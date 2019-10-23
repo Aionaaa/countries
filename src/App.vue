3cd01ceafa258d9b79668c1bdf14dd124163bc26
Iona Grant <ionagrant92@gmail.com>
2019-10-23 15:45:05 +0100

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>Countries</h1>
+    <div class="main-container">
+    <countries-list :countries="countries"></countries-list>
+    <country-detail :country="selectedCountry"></country-detail>
+    <div id="filterInput">
+      <h3>Filter Countries</h3>
+      <select id="text" v-model="filteredCountries">
+        <option v-for="country in countries" :value="country">{{country.name}}</option>
+      </select>
+    </div>
+    <select class="country" v-for="country in countries">
+      <h3>{{country.name}}</h3>
+      <p>{{country.capital}}</p>
+      <p>{{country.population}}</p>
+      <img id="flagImg" :src="country.flag">
+    </select>
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from './components/CountriesList.vue';
+import CountryDetail from './components/CountryDetail.vue';
+import {eventBus} from './main.js';
 
 export default {
   name: 'app',
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
+  mounted(){
+    eventBus.$on("country-selected", (country) => {
+      this.selectedCountry = country;
+    })
+
+    eventBus.$on("filteredCountries", (name) => {
+      this.countries.filter(country => this.country.name)
+    })
+
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+  },
   components: {
-    HelloWorld
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="css" scoped>
+
+.main-container {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div v-if="country">
+    <h3>{{country.name}}</h3>
+    <p>{{country.capital}}</p>
+    <p>{{country.population}}</p>
+    <img id="flagImg" :src="country.flag">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+#flagImg{
+  width: 250px;
+}
+</style>
